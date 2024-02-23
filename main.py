6a03c909a634aa796c6d01d86d368a3a27b14c7f
devwsl <f1d.pro.luca@gmail.com>
2024-02-23 21:31:04 +0000

    print report

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    book = "frankenstein.txt"
+    with open(f"books/{book}") as f:
         content = f.read()
-        print(content)
-        print("WORD:COUNT::: " + str(word_count(content)))
-        print(count_distinct_characters(content))
+        print_report(book.split(".")[0].upper(), content)
+        
+
 
 def word_count(text):
     return len(text.split())
@@ -17,5 +18,29 @@ def count_distinct_characters(text):
             chars[char.lower()] = 1
     return chars
 
+def sort_on(dict):
+    return dict["name"]
+
+def convert_dict_to_list(dict):
+    list = []
+    for item in dict.keys():
+            if item.isalpha():
+                list.append({"name": item, "num": dict[item]})
+    return list
+
+def print_report(title, content):
+    print("-------- BEGIN REPORT --------")
+    print(f"BOOK: {title}\n")
+    print(f"WORD COUNT: {str(word_count(content))}\n")
+
+    chars = count_distinct_characters(content)
+    
+    letters = convert_dict_to_list(chars)
+    letters.sort(reverse=False, key=sort_on)
+    
+    for letter in letters:
+        print(f"Letter '{letter["name"]}' found {letter["num"]} times.")
+
+    print("\n--------- END REPORT ---------")
 
 main()
