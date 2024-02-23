e50e05b16cefa50d7b0f283a34e4a9065c017a26
devwsl <f1d.pro.luca@gmail.com>
2024-02-23 20:58:59 +0000

    count distinct characters in text

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ def main():
         content = f.read()
         print(content)
         print("WORD:COUNT::: " + str(word_count(content)))
+        print(count_distinct_characters(content))
 
 def word_count(text):
     return len(text.split())
+
+def count_distinct_characters(text):
+    chars = {}
+    for char in text:
+        if char.lower() in chars:
+            chars[char.lower()] += 1
+        else:
+            chars[char.lower()] = 1
+    return chars
+
 
 main()
